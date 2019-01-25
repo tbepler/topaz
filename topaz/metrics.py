4ed4e6e0f7a2f21db592ecc30434868eeb75deaf
Tristan Bepler <tbepler@gmail.com>
2019-01-25 17:41:55 -0500

    updated metrics to be faster with pure numpy implementation

--- topaz/metrics.py
+++ topaz/metrics.py
@@ -11,30 +11,38 @@ def precision_recall_curve(target, pred, N=None):
     ## copy the target and prediction into matrix
     matrix = np.stack([-pred,target], 1)
     matrix.view('f4,f4').sort(order='f0', axis=0) # sort the rows
-    
-    precision = np.zeros(target.shape[0], dtype=matrix.dtype)
-    recall = np.zeros(target.shape[0], dtype=matrix.dtype)
-    threshold = np.zeros(target.shape[0], dtype=matrix.dtype)
 
-    auprc = count = pr = relk = rel = 0
-    j = 0
+    # if we have duplicate predictions (i.e. targets with the same score)
+    # compress these into one bucket with k entries and r hits
     
-    for i in range(matrix.shape[0]):
-        count += 1
-        rel += matrix[i,1]
-        relk += matrix[i,1] # target
-        delta = matrix[i,1] - pr
-        pr += delta/count
-        if i >= matrix.shape[0] - 1 or matrix[i,0] != matrix[i+1,0]:
-            precision[j] = pr
-            recall[j] = rel/n
-            threshold[j] = -matrix[i,0]
-            j += 1
-            auprc += pr*relk
-            relk = 0
-    auprc /= n
+    # find where score[i] != score[i+1]
+    mask = np.zeros(len(matrix), dtype=bool)
+    mask[:-1] = (matrix[:-1,0] != matrix[1:,0])
+    mask[-1] = True # last value is always true
+
+    # how many blocks are there
+    blocks = np.sum(mask)
+    # count elements per block
+    counts = np.zeros(blocks+1, dtype=int)
+    counts[1:] = np.where(mask)[0] + 1 # last index of each block + 1
+    k = np.diff(counts)
+    pp = counts[1:] # number of predicted positives at each bucket
+
+    # count true positives per block
+    tp = np.cumsum(matrix[:,1])
+    counts = np.zeros(blocks+1, dtype=int)
+    counts[1:] = tp[mask]
+    r = np.diff(counts)
+    tp = counts[1:] # numer of true positives at each bucket
     
-    return precision[:j], recall[:j], threshold[:j], auprc
+    pr = tp/pp # precision at each bucket
+    avpr = np.sum(pr*r)/n # average-precision score = sum_buckets [precision(bucket)*recall(bucket)]
+    
+    re = r/n # recall at each bucket 
+    threshold = matrix[:,0][mask] # threshold of each bucket
+
+    return pr, re, threshold, avpr
+
 
 def average_precision(target, pred, N=None):
     if N is None:
@@ -45,18 +53,32 @@ def average_precision(target, pred, N=None):
     ## copy the target and prediction into matrix
     matrix = np.stack([-pred,target], 1)
     matrix.view('f4,f4').sort(order='f0', axis=0) # sort the rows
+
+    # if we have duplicate predictions (i.e. targets with the same score)
+    # compress these into one bucket with k entries and r hits
     
-    auprc = count = pr = relk = 0
+    # find where score[i] != score[i+1]
+    mask = np.zeros(len(matrix), dtype=bool)
+    mask[:-1] = (matrix[:-1,0] != matrix[1:,0])
+    mask[-1] = True # last value is always true
+
+    # how many blocks are there
+    blocks = np.sum(mask)
+    # count elements per block
+    counts = np.zeros(blocks+1, dtype=int)
+    counts[1:] = np.where(mask)[0] + 1 # last index of each block + 1
+    k = np.diff(counts)
+    pp = counts[1:] # number of predicted positives at each bucket
+
+    # count true positives per block
+    tp = np.cumsum(matrix[:,1])
+    counts = np.zeros(blocks+1, dtype=int)
+    counts[1:] = tp[mask]
+    r = np.diff(counts)
+    tp = counts[1:] # numer of true positives at each bucket
     
-    for i in range(matrix.shape[0]):
-        count += 1
-        relk += matrix[i,1] # target
-        delta = matrix[i,1] - pr
-        pr += delta/count
-        if i >= matrix.shape[0] - 1 or matrix[i,0] != matrix[i+1,0]:
-            auprc += pr*relk
-            relk = 0
-    auprc /= n
-    
-    return auprc
+    pr = tp/pp # precision at each bucket
+    avpr = np.sum(pr*r)/n # average-precision score = sum_buckets [precision(bucket)*recall(bucket)]
+
+    return avpr
 
