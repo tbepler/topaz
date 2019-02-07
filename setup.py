fd186bf3fabc6899f8bb10a6d61253015628daf9
tbepler <tbepler@gmail.com>
2019-02-06 19:10:12 -0500

    added metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
 from setuptools import setup, find_packages
 
+name = 'topaz'
+version = '0.0.0'
+
+description = 'Particle picking with positive-unlabeled CNNs'
+long_description = 'Particle picking software for single particle cryo-electron microscopy using convoluational neural networks and positive-unlabeled learning.'
+
+keywords = 'cryoEM particle-picking CNN positive-unlabeled topaz'
+
+url = 'https://github.com/tbepler/topaz'
+
+author = 'Tristan Bepler'
+author_email = 'tbepler@mit.edu'
+
+license = 'GPLv3'
+
 setup(
-    name = 'topaz',
+    name = name,
+    version=version,
+    description=description,
+    long_description=long_description,
+    keywords=keywords,
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
+
     packages=find_packages(),
     #package_dir = {'': 'topaz'},
     entry_points = {'console_scripts': ['topaz = topaz.main:main']},
+    include_package_data = True
 )
