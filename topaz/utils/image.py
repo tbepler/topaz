c64da8b8fc2984d888b33ae7d6cacaf63dd4772f
Darnell Granberry <40174000+DarnellGranberry@users.noreply.github.com>
2022-05-13 14:49:35 -0400

    refactored image downsample

--- topaz/utils/image.py
+++ topaz/utils/image.py
@@ -3,8 +3,11 @@ from __future__ import division, print_function
 import numpy as np
 from PIL import Image
 import os
+import sys
 
 import topaz.mrc as mrc
+from topaz.utils.data.loader import load_image
+
 
 def downsample(x, factor=1, shape=None):
     """ Downsample 2d array using fourier transform """
@@ -31,6 +34,28 @@ def downsample(x, factor=1, shape=None):
 
     return f.astype(x.dtype)
 
+
+def downsample_file(path:str, scale:int, output:str, verbose:bool):
+    ## load image
+    im = load_image(path)
+    # convert PIL image to array
+    im = np.array(im, copy=False).astype(np.float32)
+
+    small = downsample(im, scale)
+
+    if verbose:
+        print('Downsample image:', path, file=sys.stderr)
+        print('From', im.shape, 'to', small.shape, file=sys.stderr)
+
+    # write the downsampled image
+    with open(output, 'wb') as f:
+        im = Image.fromarray(small)
+        if small.dtype == np.uint8:
+            im.save(f, 'png')
+        else:
+            im.save(f, 'tiff')
+
+
 def quantize(x, mi=-3, ma=3, dtype=np.uint8):
     if mi is None:
         mi = x.min()
@@ -41,14 +66,14 @@ def quantize(x, mi=-3, ma=3, dtype=np.uint8):
     x = np.clip(x, 0, 255)
     x = np.round(x).astype(dtype)
     return x
-    #buckets = np.linspace(mi, ma, 255)
-    #return np.digitize(x, buckets).astype(dtype)
+
 
 def unquantize(x, mi=-3, ma=3, dtype=np.float32):
     """ convert quantized image array back to approximate unquantized values """
     x = x.astype(dtype)
     y = x*(ma-mi)/255 + mi
     return y
+
 
 def save_image(x, path, mi=-3, ma=3, f=None, verbose=False):
     if f is None:
@@ -69,19 +94,23 @@ def save_image(x, path, mi=-3, ma=3, f=None, verbose=False):
     elif f == 'jpg' or f == 'jpeg':
         save_jpeg(x, path, mi=mi, ma=ma)
 
+
 def save_mrc(x, path):
     with open(path, 'wb') as f:
         x = x[np.newaxis] # need to add z-axis for mrc write
         mrc.write(f, x)
 
+
 def save_tiff(x, path):
     im = Image.fromarray(x) 
     im.save(path, 'tiff')
+
 
 def save_png(x, path, mi=-3, ma=3):
     # byte encode the image
     im = Image.fromarray(quantize(x, mi=mi, ma=ma))
     im.save(path, 'png')
+
 
 def save_jpeg(x, path, mi=-3, ma=3):
     # byte encode the image
